eed5b820a9c429b73cdac6028b7e1d98703790f1
jeancarlo92 <armas_plus44@hotmail.com>
2014-11-28 20:44:34 -0500

    cambio

--- caracter.py
+++ caracter.py
@@ -1,19 +1,20 @@
 k=int(input("Ingrese numero de caracteres: "))
-cad=raw_input("Ingrese cadena: ")
+cad=input("Ingrese cadena: ")
 
 cont=0
 palabras=0
 for caracter in cad:
    if caracter != ' ':
      cont+=1
-     print(cont,caracter)
+     print(cont, caracter)
+     if cont==k and cad[cont]==' ':
+        palabras+=1
+        cont=0
+     #else:
+     #    if cont==k:
+     #       palabras+=1
    else:
-       if cont==k or cad[0:-1]!='/0':
-          palabras+=1
-          cont=0
-       else:
-          cont=0
+      cont=0
 
 
 print("Hay %d palabras con una longitud de %d caracteres " % (palabras, k ))
-
