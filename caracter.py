c7d7f8bfce29e4104e98e0479b20dfb4aed6ef26
jeancarlo92 <armas_plus44@hotmail.com>
2014-11-29 00:49:59 -0500

    Update caracter.py

--- caracter.py
+++ caracter.py
@@ -7,14 +7,19 @@ for caracter in cad:
    if caracter != ' ':
      cont+=1
      print(cont, caracter)
-     if cont==k and cad[cont]==' ':
+   else:  
+      if cont==k:
         palabras+=1
         cont=0
-     #else:
-     #    if cont==k:
-     #       palabras+=1
-   else:
-      cont=0
+     else:
+        cont=0
+if len(cad)==k:
+     palabras+=1
+     print(palabras)
+else:
+  if cad[-k]!=' ' and cad[-k-1]==' ':
+     palabras+=1
+     print(palabras)
 
 
 print("Hay %d palabras con una longitud de %d caracteres " % (palabras, k ))
